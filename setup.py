6a44bc12194b915453f6189b5f8168d0c520df78
bozok17 <bozok17@gmail.com>
2025-07-28 14:45:29 +0300

    install requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='medical-rag',
+    version='0.1.0',
+    author='Fatih Aydin',
+    author_email='bozok17@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
